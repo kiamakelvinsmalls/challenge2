260028165118b6c82e58db51502f0f08d168e492
kiamakelvin <kiamakelvin@gmail.com>
2017-10-22 05:13:51 +0300

    add a test file

---
+++ ui/mytest.py
@@ -0,0 +1,18 @@
+import unittest
+from yummy import login
+class loginTestCase(unittest.TestCase):
+	"""docstring for loginTestCase"""
+	def test_login(self):
+		"test if login is good"
+		self.assertTrue(login)
+if __name__=='__main__':
+	 unittest.main()
+
+from yummy import recipes
+class loginTestCase(unittest.TestCase):
+	"""docstring for loginTestCase"""
+	def test_recipes(self):
+		"test if login is good"
+		self.assertTrue(recipes)
+if __name__=='__main__':
+	 unittest.main()
